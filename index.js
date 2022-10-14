4585d5ffc67393808b8aa3f4c2c038fe5fa2e1a1
divyej <67976979+divyej@users.noreply.github.com>
2022-10-14 10:12:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ button.addEventListener("click", function like(){
   } else {
     button.classList.add("liked")
     
-    var deadline = new Date("Oct 11, 2022 09:11:00").getTime();
+    var deadline = new Date("Oct 11, 2023 09:11:00").getTime();
 var x = setInterval(function() {
 var now = new Date().getTime();
 var t = deadline - now;
@@ -24,8 +24,7 @@ html.textContent = days + "d "
 + hours + "h " + minutes + "m " + seconds + "s ";
     if (t < 0) {
         clearInterval(x);
-        document.getElementById("demo").textContent = "Hey , Aashika jain ,You know i worry alot, earlier i went down the lake and sat there alone with my thoughts, I felt like someone without a home, a nobody, but the kind of love you give me just keeps me motivated, all I will ever need is you, thank you for being there for me, thankyou for being my home.I just wanna let you know that the bond we share is something that i cherish everyday , i wake up with a happy face and sleep even happier , i sing songs aloud and dance to them too and this newfound confidence is just because of you .I may be happier earlier but i am transforming to a even better person every single day. You too are a whole package and  today we will finally meet . I love you so much!"
-    }
+        document.getElementById("demo").textContent = "H"
 }, 1000); 
   }
 })
